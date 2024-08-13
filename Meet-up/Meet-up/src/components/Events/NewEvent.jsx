98551b05a0aabf33071ab3e14b88d7fefff17ff3
dhruvi6071 <dhruvi6071@gmail.com>
2024-08-13 16:39:07 +0530

    changing-data-with-mutation

--- Meet-up/Meet-up/src/components/Events/NewEvent.jsx
+++ Meet-up/Meet-up/src/components/Events/NewEvent.jsx
@@ -1,12 +1,22 @@
 import { Link, useNavigate } from 'react-router-dom';
-
+import { useMutation } from '@tanstack/react-query'; //To send the post request manually or only when we want use useQuery.
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { createNewEvent } from '../../utill/http.js';
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  //This function is called whenever any send request manually whne this component is run.
+  const {mutate} = useMutation({
+
+    mutationFn: createNewEvent
+  });
+
+  function handleSubmit(formData) {
+    mutate({event: formData}); 
+  }
+
 
   return (
     <Modal onClose={() => navigate('../')}>
